7313330653abbf52be181e694c8e065681162e2f
Kevin Juneja <kevin.j.24@gmail.com>
2023-02-16 14:37:37 -0800

    set default photo quality as nothing

--- custom-templates/image-grids/flex-dest/dev/js/components/LeftPanel.jsx
+++ custom-templates/image-grids/flex-dest/dev/js/components/LeftPanel.jsx
@@ -7,7 +7,7 @@ export default function LeftPanel({
   labeledPhotoQualityTier,
   onSubmitOrSkip,
 }) {
-  const [photoQualityTier, setPhotoQualityTier] = useState('Most Inspiring');
+  const [photoQualityTier, setPhotoQualityTier] = useState('');
   const [isSaving, setIsSaving] = useState(false);
   const [isSkipping, setIsSkipping] = useState(false);
 
@@ -24,7 +24,7 @@ export default function LeftPanel({
 
     e.preventDefault();
     Labelbox.skip().then(() => {
-      setPhotoQualityTier('Most Inspiring');
+      setPhotoQualityTier('');
       e.target.blur();
       Labelbox.fetchNextAssetToLabel();
       setIsSkipping(false);
@@ -43,7 +43,7 @@ export default function LeftPanel({
     };
 
     Labelbox.setLabelForAsset(JSON.stringify(formattedData)).then(() => {
-      setPhotoQualityTier('Most Inspiring');
+      setPhotoQualityTier('');
       if (!labeledPhotoId) {
         Labelbox.fetchNextAssetToLabel();
       }
@@ -129,7 +129,7 @@ export default function LeftPanel({
           {isSkipping ? 'Skipping...' : 'Skip Listing'}
         </button>
         <button
-          disabled={isSaving || isSkipping}
+          disabled={isSkipping || isSaving || photoQualityTier === ''}
           className="cta save-cta"
           type="submit"
           onClick={(e) => handleSubmit(e)}
